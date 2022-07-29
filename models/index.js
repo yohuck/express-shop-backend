83c4935483662426d3860a6980b1995154d5c702
Eric Lake <ericallenlake@gmail.com>
2022-07-28 22:45:31 -0600

    Typo fix

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: {model: ProductTag, uniquie: false,}, foreignKey: 'product_id'});
+Product.belongsToMany(Tag, {through: {model: ProductTag, unique: false,}, foreignKey: 'product_id'});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {through: {model: ProductTag, unique: false}, foreignKey: 'tag_id'});
 
