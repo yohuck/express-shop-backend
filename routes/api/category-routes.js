9c690eba1d4eb5b254eac0490605832ef8bc5fe9
Eric Lake <ericallenlake@gmail.com>
2022-07-29 13:50:38 -0600

    Adds put routes for category and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,7 +44,23 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      { 
+        where: {category_id: req.params.id }
+      })
+  
+      if (!categoryData) {
+          res.status(404).json({message: 'No category found with that id!'});
+          return
+      }
+      res.status(200).json(categoryData)
+  } catch(err){
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -12,8 +12,6 @@ router.get('/', async (req, res) => {
   } catch(err){
     res.status(500).json(err)
   }
-  // find all tags
-  // be sure to include its associated Product data
 });
 
 router.get('/:id', async (req, res) => {
@@ -31,8 +29,6 @@ router.get('/:id', async (req, res) => {
   } catch(err){
     res.status(500).json(err);
   }
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
 });
 
 router.post('/', async (req, res) => {
@@ -46,8 +42,24 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      { 
+        where: {tag_id: req.params.id }
+      })
+  
+      if (!tagData) {
+          res.status(404).json({message: 'No tag found with that id!'});
+          return
+      }
+      res.status(200).json(tagData)
+  } catch(err){
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -66,7 +78,6 @@ router.delete('/:id', async (req, res) => {
   } catch (err){
     res.status(500).json(err);
   }
-  // delete on tag by its `id` value
 });
 
 module.exports = router;
